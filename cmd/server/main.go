46fbcb5b97df1a27c655df7413b2b2c6003a944d
Jonilson Sousa <jonilsonds9@gmail.com>
2025-07-20 10:53:30 -0300

    Criando handler para criar produto

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,8 +1,56 @@
 package main
 
-import "github.com/jonilsonds9/goexpert-modulo-7-apis/configs"
+import (
+	"encoding/json"
+	"github.com/jonilsonds9/goexpert-modulo-7-apis/configs"
+	"github.com/jonilsonds9/goexpert-modulo-7-apis/internal/dto"
+	"github.com/jonilsonds9/goexpert-modulo-7-apis/internal/entity"
+	"github.com/jonilsonds9/goexpert-modulo-7-apis/internal/infra/database"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"net/http"
+)
 
 func main() {
-	config, _ := configs.LoadConfig(".")
-	println(config.DBDriver)
+	_, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	db.AutoMigrate(&entity.Product{}, &entity.User{})
+
+	http.ListenAndServe(":8000", nil)
+}
+
+type ProductHandler struct {
+	ProductDB database.ProductInterface
+}
+
+func NewProductHandler(db database.ProductInterface) *ProductHandler {
+	return &ProductHandler{
+		ProductDB: db,
+	}
+}
+
+func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
+	var product dto.CreateProductInput
+	err := json.NewDecoder(r.Body).Decode(&product)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	p, err := entity.NewProduct(product.Name, product.Price)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	err = h.ProductDB.Create(p)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
 }
