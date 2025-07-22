2c42a08b2fb6a9bf769f62871ccb1d21513cb269
Jonilson Sousa <jonilsonds9@gmail.com>
2025-07-21 22:39:22 -0300

    Protegendo products

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/jwtauth"
 	"github.com/jonilsonds9/goexpert-modulo-7-apis/configs"
 	"github.com/jonilsonds9/goexpert-modulo-7-apis/internal/entity"
 	"github.com/jonilsonds9/goexpert-modulo-7-apis/internal/infra/database"
@@ -30,11 +31,16 @@ func main() {
 
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Post("/products", productHandler.CreateProduct)
-	r.Get("/products", productHandler.GetProducts)
-	r.Get("/products/{id}", productHandler.GetProduct)
-	r.Put("/products/{id}", productHandler.UpdateProduct)
-	r.Delete("/products/{id}", productHandler.DeleteProduct)
+
+	r.Route("/products", func(r chi.Router) {
+		r.Use(jwtauth.Verifier(configs.TokenAuth))
+		r.Use(jwtauth.Authenticator)
+		r.Post("/", productHandler.CreateProduct)
+		r.Get("/", productHandler.GetProducts)
+		r.Get("/{id}", productHandler.GetProduct)
+		r.Put("/{id}", productHandler.UpdateProduct)
+		r.Delete("/{id}", productHandler.DeleteProduct)
+	})
 
 	r.Post("/users", userHandler.CreateUser)
 	r.Post("/users/generate-token", userHandler.GetJWt)
