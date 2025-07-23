f2b243b9050167f83d8dd2ee9871966ae261cfb2
Jonilson Sousa <jonilsonds9@gmail.com>
2025-07-22 22:03:18 -0300

    Gerando primeira documentação

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -14,6 +14,23 @@ import (
 	"net/http"
 )
 
+// @title           GoExpert Modulo 7 APIs
+// @version         1.0
+// @description     API for managing products and users.
+// @termsOfService  http://swagger.io/terms/
+
+// @contact.name   API Support
+// @contact.url    http://www.swagger.io/support
+// @contact.email	jonilson@mail.com
+
+// @license.name  Apache 2.0
+// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host      localhost:8000
+// @BasePath  /
+// @securityDefinitions.apiKey ApiKeyAuth
+// @in header
+// @name Authorization
 func main() {
 	configs, err := configs.LoadConfig(".")
 	if err != nil {
